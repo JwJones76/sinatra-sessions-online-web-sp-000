9ef04e5e7a73574aad3eccbac6ba2d227c07e55c
Jacob W Jones <jacobwadejones76@gmail.com>
2021-05-21 18:00:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -37,10 +37,10 @@ class App < Sinatra::Base
   end
 
   get '/set_session' do
-    session[:user_id] = 1
     session[:id] = 1
     
-    if session[:user_id] == 1
+    
+    if session[:id] == 1
       
       redirect '/fetch_session_id'
     else
@@ -49,7 +49,7 @@ class App < Sinatra::Base
   end
 
   get '/fetch_session_id' do
-    "You did it! session[:user_id] value: #{session[:user_id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
+    "You did it! session[:user_id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
   end
 
   get '/logout' do
